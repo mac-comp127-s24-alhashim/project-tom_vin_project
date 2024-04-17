fd988b1af4167627187749f1404b6b6618fadd42
tandreae <tandreae@macalester.edu>
2024-04-17 11:24:53 -0500

    Update

--- src/Connect4/GameBoard.java
+++ src/Connect4/GameBoard.java
@@ -24,8 +24,6 @@ public class GameBoard {
         return false; // Column is full
     }
 
-    public boolean checkWin(Token token
-    }
-testint testing
+    public boolean checkWin(Token token){ }
     // Add additional methods for isFull, isEmpty, checkWin, checkTie later.
 }
