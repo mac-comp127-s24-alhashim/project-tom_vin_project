fd5a242e1f1cd7803c6bf97fafdde2dce462b842
tandreae <tandreae@macalester.edu>
2024-04-17 11:45:02 -0500

    Check wins
    Added check vertical and horizontal win

--- src/Connect4/GameBoard.java
+++ src/Connect4/GameBoard.java
@@ -24,6 +24,36 @@ public class GameBoard {
         return false; // Column is full
     }
 
-    public boolean checkWin(Token token){ }
+    public boolean checkWin(Token token){
+        return checkHorizontalWin(token) || checkVerticalWin(token) || checkDiagonalWin(token);
+     }
+
+     // Check for horizontal win
+     private boolean checkHorizontalWin(Token token) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns - 3; col++) {
+                if (grid[row][col] == token && grid[row][col + 1] == token && 
+                    grid[row][col + 2] == token && grid[row][col + 3] == token) {
+                        return true; // horizontal win met
+                    }
+            }
+        }
+        return false;
+     }
+
+     // Check for vertical win
+     private boolean checkVerticalWin(Token token) {
+        for (int col = 0; col < col; col++) {
+            for (int row = 0; row < row - 3; row++) {
+                if (grid[row][col] == token && grid[row + 1][col] == token &&
+                    grid[row + 2][col] == token && grid[row + 3][col] == token) {
+                        return true; // vertical win met
+                    }
+            }
+        }
+        return false;
+     }
+
+
     // Add additional methods for isFull, isEmpty, checkWin, checkTie later.
 }
